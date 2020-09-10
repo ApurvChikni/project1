f62772f92821f465f78d72f24f8a752ff89c630f
apurv1306200 <48054297+apurv1306200@users.noreply.github.com>
2020-09-10 19:45:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def api(full_path):
     return predicted
 #FOR THE SECOND MODEL
 def api1(full_path):
-    data = image.load_img(full_path, target_size=(224, 224, 3))
+    data = image.load_img(full_path, target_size=(64, 64, 3))
     data = np.expand_dims(data, axis=0)
     data = data * 1.0 / 255
 
